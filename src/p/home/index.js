251fcada27ba3ae61f41e79dcb30906292a541f4
小虎 <13521602062@163.com>
2018-12-03 23:27:16 +0800

    push

---
+++ src/p/home/index.js
@@ -0,0 +1,26 @@
+require('@br/lib/zepto.min');
+var _ = require('underscore');
+function Index() {
+    this.init.apply(this, arguments);
+}
+
+_.extend(Index.prototype, {
+    init:function(){
+    }
+});
+new Index();
+        
+
+         
+    
+
+
+
+
+
+
+
+
+
+
+
